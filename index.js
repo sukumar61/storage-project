0b26523cc00f1e02d6c66705e0e2bae8fd7881a7
SukumarRenukuntla <sukumarrenukuntla@gmail.com>
2026-03-12 01:29:53 +0530

    add gitignore and update project files

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from "express"
+import cors from 'cors'
+import bcrypt from "bcrypt"
+
 
 const app=express()
+app.use(cors({origin:"*"}))
+app.use(express.json())
+app.use(express.urlencoded({extension:true}))
+
+
 app.listen(3000,()=>{
     console.log("server started")
 })
 
-app.get("/",(req,res)=>{
+app.get("/", (req,res)=>{
     res.status(200).json({name:"ashkulu puli"})
+})
+
+app.post("/register",async (req,res)=>{
+    const name="aishu"
+    const email="aishukasam@gmail.com"
+    const password="aishu@puli"
+    const hashedpassword= await bcrypt.hash(password,10)
+    console.log(hashedpassword)
 })
